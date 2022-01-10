9e5186abfb7a3b492234213ce9e02c61c5668ec0
Salatiel F <95117339+SalatielF@users.noreply.github.com>
2022-01-09 23:15:50 -0400

    salatiel

---
+++ JavaScript/app2/jogo.js
@@ -0,0 +1,13 @@
+	
+var altura = 0
+var largra = 0
+
+function ajustaTamanhoPalcoJogo () {
+	altura = window.innerHeight
+	largura = window.innerWidth
+
+	console.log(altura, largura)
+}
+
+ajustaTamanhoPalcoJogo()
+
