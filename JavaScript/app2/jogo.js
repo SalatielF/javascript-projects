5db6dbeaf95742fcc31269dbdbe5b524eb68a361
Salatiel F <95117339+SalatielF@users.noreply.github.com>
2022-01-10 16:34:36 -0400

    salatiel

--- JavaScript/app2/jogo.js
+++ JavaScript/app2/jogo.js
@@ -6,8 +6,18 @@ function ajustaTamanhoPalcoJogo () {
 	altura = window.innerHeight
 	largura = window.innerWidth
 
-	console.log(altura, largura)
+	console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
 
+var posicaoX = Math.floor (Math.random() * largura)
+var posicaoY = Math.floor (Math.random() * altura)
+
+console.log(posicaoX, posicaoY)
+
+
+//criar o elemento html
+var mosquito = document.creatElement('img')
+
+document.body.appendChild()
