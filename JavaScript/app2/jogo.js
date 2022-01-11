d44a4f0c44afed8861acf80b8f0eddec0077a7f7
Salatiel F <95117339+SalatielF@users.noreply.github.com>
2022-01-11 14:12:21 -0400

    salatiel

--- JavaScript/app2/jogo.js
+++ JavaScript/app2/jogo.js
@@ -1,6 +1,7 @@
 	
 var altura = 0
 var largra = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo () {
 	altura = window.innerHeight
@@ -16,6 +17,15 @@ function posicaoRandomica() {
 	//remover o mosquito anterior (caso exista)
 	if(document.getElementById('mosquito')) {
 		document.getElementById('mosquito').remove()
+
+		//console.log('elemento selecionado foi: v' + vidas)
+		if(vidas > 3) {
+
+			alert('Interromper o jogo (game over)')
+		} else {
+			document.getElementById('v' + vidas).src= "imagens/coracao_vazio.png"
+
+		}	vidas++
 	}
 
 	var posicaoX = Math.floor (Math.random() * largura) - 90
@@ -34,6 +44,9 @@ function posicaoRandomica() {
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 	mosquito.id = 'mosquito'
+	mosquito.onclick = function() {
+		this.remove()
+	}
 
 	document.body.appendChild(mosquito)
 
