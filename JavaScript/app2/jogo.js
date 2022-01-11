291606ee71c2633560a14bd62bc3baf8319f91ea
Salatiel F <95117339+SalatielF@users.noreply.github.com>
2022-01-11 14:29:20 -0400

    salatiel

--- JavaScript/app2/jogo.js
+++ JavaScript/app2/jogo.js
@@ -21,7 +21,7 @@ function posicaoRandomica() {
 		//console.log('elemento selecionado foi: v' + vidas)
 		if(vidas > 3) {
 
-			alert('Interromper o jogo (game over)')
+			window.location.href = 'fim_de_jogo.html'
 		} else {
 			document.getElementById('v' + vidas).src= "imagens/coracao_vazio.png"
 
