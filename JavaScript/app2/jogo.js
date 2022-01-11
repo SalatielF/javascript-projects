62473e1d608a484672d51f15b067dfe70bbdacf9
Salatiel F <95117339+SalatielF@users.noreply.github.com>
2022-01-11 15:23:31 -0400

    salatiel

--- JavaScript/app2/jogo.js
+++ JavaScript/app2/jogo.js
@@ -4,6 +4,22 @@ var largra = 0
 var vidas = 1
 var tempo = 15
 
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal') {
+	//1150
+	criaMosquitoTempo = 1500
+} else if(nivel === 'dificil') {
+	//1000
+	criaMosquitoTempo = 1000
+} else if(nivel === 'chucknorris') {
+	//750
+	criaMosquitoTempo = 750
+}
+
 function ajustaTamanhoPalcoJogo () {
 	altura = window.innerHeight
 	largura = window.innerWidth
